42dd1af69c13f7de4abbb388c7cd7aba883c8677
sadi-tanvir <htanvir.sadi@gmail.com>
2024-07-06 17:53:07 +0600

    feat: add Javascript and PHP solutions for 3065. Minimum Operations to Exceed Threshold Value I

---
+++ PHP - Solutions/3065. MinimumOperationstoExceedThresholdValueI.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>3065. MinimumOperationstoExceedThresholdValueI</title>
+</head>
+
+<body>
+    <h1>3065. Minimum Operations to Exceed Threshold Value I</h1>
+
+    <?php
+    class Solution
+    {
+        function minOperations($nums, $k) {
+            $count = 0;
+            for ($i = 0; $i < count($nums); $i++) if ($k > $nums[$i]) $count++;
+            return $count;
+        }
+    }
+
+    $solution = new Solution();
+    echo '<pre>' . json_encode($solution->minOperations([2, 11, 10, 1, 3], 10)) . '</pre>';
+    echo '<pre>' . json_encode($solution->minOperations([1, 1, 2, 4, 9], 1)) . '</pre>';
+    ?>
+
+    <!-- php -S localhost:8000 -->
+
+</body>
+
+</html>
