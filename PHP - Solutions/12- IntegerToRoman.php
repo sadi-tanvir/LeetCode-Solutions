9ba25043812e2e6f59f442736467b9f0cf8bd3b9
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2024-10-20 07:40:59 +0600

    feat(JS): Add JavaScript solution for '222. Count Complete Tree Nodes'

--- PHP - Solutions/12- IntegerToRoman.php
+++ PHP - Solutions/12- IntegerToRoman.php
@@ -4,18 +4,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>12. Integer to Roman</title>
+    <title>67. Add Binary</title>
 </head>
 
 <body style="width: 100vw; height: 100vh; background: #000; color: white">
-    <h1>12. Integer to Roman</h1>
+    <h1>67. Add Binary</h1>
 
-    <!-- https://leetcode.com/problems/integer-to-roman/description/ -->
+    <!-- https://leetcode.com/problems/add-binary/ -->
 
     <?php
     class Solution
     {
-        function intToRoman ($num) {
+        function intToRoman($num)
+        {
             $romanNumeralsMap = [[1000, "M"], [900, "CM"], [500, "D"], [400, "CD"], [100, "C"], [90, "XC"], [50, "L"], [40, "XL"], [10, "X"], [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]];
 
             $currentIndex = 0;
